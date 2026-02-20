819f69237d9ccb33d494884ab4bfe28297798225
Alistair Smith <hi@alistair.sh>
2026-02-20 07:23:00 +0000

    Fix CookieMap.get return type compilation error

--- Sources/FuzzilliCli/Profiles/BunProfile.swift
+++ Sources/FuzzilliCli/Profiles/BunProfile.swift
@@ -864,7 +864,7 @@ public let bunCookieMapGroup = ObjectGroup(
     instanceType: .bunCookieMap,
     properties: [:],
     methods: [
-        "get":     [.string] => .bunCookie | .undefined,
+        "get":     [.string] => .jsAnything,
         "set":     [.string, .string, .opt(.object())] => .undefined,
         "delete":  [.string] => .boolean,
         "has":     [.string] => .boolean,
